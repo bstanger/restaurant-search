edba5b9ee84e709bbf888868b714e482220eb7e7
Becca Stanger <beccastanger@gmail.com>
2018-06-25 20:50:49 -0700

    Alter empty state for results

--- project-files/assets/components/SearchBody.jsx
+++ project-files/assets/components/SearchBody.jsx
@@ -9,28 +9,30 @@ class SearchBody extends React.Component {
   render(){
     return (
       <section className='search-body'>
-        <aside className='search-body__sidebar'>
-          <div>Cuisines</div>
-          <div>Ratings</div>
-          <div>Payments</div>
-        </aside>
-        <div className='search-body__main'>
-          {this.props.results.length === 0 ?
-            <div>No results</div>
-          :
-            <section className='search-body__results'>
-              <h3 className='search-body__results-header'>
-                {this.props.results.length} results found in {this.props.milliseconds / 1000} seconds
-              </h3>
-              <div className='search-body__results-content'>
-                {this.props.results.map((result, idx) =>
-                  <SearchResult result={result} key={idx}/>
-                )}
-              </div>
-              <button className='search-body__load'>Show More</button>
-            </section>
-          }
-        </div>
+        {this.props.results.length === 0 ?
+          <div className='search-body__empty'>No results</div>
+        :
+          <div className='search-body__not-empty'>
+            <aside className='search-body__sidebar'>
+              <div>Cuisines</div>
+              <div>Ratings</div>
+              <div>Payments</div>
+            </aside>
+            <div className='search-body__main'>
+              <section className='search-body__results'>
+                <h3 className='search-body__results-header'>
+                  {this.props.results.length} results found in {this.props.milliseconds / 1000} seconds
+                </h3>
+                <div className='search-body__results-content'>
+                  {this.props.results.map((result, idx) =>
+                    <SearchResult result={result} key={idx}/>
+                  )}
+                </div>
+                <button className='search-body__load'>Show More</button>
+              </section>
+            </div>
+          </div>
+        }
       </section>
     )
   }
